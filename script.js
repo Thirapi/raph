95e3ceb813ea6e9c8e0e53ded51c47847c956e06
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-05-11 05:56:25 +0000

    jaga jaga

--- script.js
+++ script.js
@@ -1,220 +1,80 @@
-const start = document.querySelector("[data-start]");
-const windows =  [... document.querySelectorAll("[data-window]")];
-const triggers = [... document.querySelectorAll("[data-window-trigger]")];
-const errors = [... document.querySelectorAll("[data-error-trigger]")];
+document.addEventListener('DOMContentLoaded', function() {
+    // Menangani klik tombol Start
+    document.getElementById('startButton').addEventListener('click', function(e) {
+        e.stopPropagation(); // Menghentikan event dari menyebabkan penyebaran (bubbling) ke elemen lain
+        toggleShutdownMenu(); // Memunculkan atau menyembunyikan menu shutdown
+    });
 
-start.addEventListener("click", e => {
-	if(start.checked) {
-		start.setAttribute("checked", "");
-	} else {
-		start.removeAttribute("checked");
-	}
-	triggers.forEach(trigger=>{
-		trigger.classList.remove("active");
-	});
+    // Fungsi untuk memunculkan atau menyembunyikan menu shutdown
+    function toggleShutdownMenu() {
+        var shutdownMenu = document.getElementById('shutdownMenu');
+        if (shutdownMenu.style.display === 'block') {
+            shutdownMenu.style.display = 'none'; // Sembunyikan menu jika sudah terbuka
+        } else {
+            shutdownMenu.style.display = 'block'; // Tampilkan menu shutdown
+        }
+    }
+
+    // Menangani klik di luar menu shutdown untuk menutup menu
+    document.addEventListener('click', function(e) {
+        var shutdownMenu = document.getElementById('shutdownMenu');
+        var startButton = document.getElementById('startButton');
+
+        // Cek apakah yang diklik bukan bagian dari menu shutdown atau tombol Start
+        if (!shutdownMenu.contains(e.target) && e.target !== startButton) {
+            shutdownMenu.style.display = 'none'; // Sembunyikan menu shutdown
+        }
+    });
+
+    // Menangani klik opsi shutdown
+    document.getElementById('shutdownOption').addEventListener('click', function(e) {
+        e.preventDefault(); // Menghentikan aksi default dari link
+        alert('Shutdown Process Placeholder!'); // Contoh: Jalankan proses shutdown
+    });
+
+    // Menangani klik opsi restart
+    document.getElementById('restartOption').addEventListener('click', function(e) {
+        e.preventDefault(); // Menghentikan aksi default dari link
+        alert('Restart Process Placeholder!'); // Contoh: Jalankan proses restart
+    });
+
+    // Fungsi untuk mengupdate dan menampilkan jam real-time
+    function updateClock() {
+        var clockElement = document.getElementById('clock');
+        var currentTime = new Date();
+
+        var hours = currentTime.getHours();
+        var minutes = currentTime.getMinutes();
+        var seconds = currentTime.getSeconds();
+
+        // Format waktu menjadi HH:MM:SS AM/PM
+        var amPM = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // Jam 0 diubah menjadi 12 (format 12-hour)
+        minutes = minutes < 10 ? '0' + minutes : minutes; // Menambahkan leading zero pada menit
+        seconds = seconds < 10 ? '0' + seconds : seconds; // Menambahkan leading zero pada detik
+
+        var clockString = hours + ':' + minutes + ':' + seconds + ' ' + amPM;
+        clockElement.textContent = clockString; // Memasukkan waktu ke dalam elemen clock
+
+        // Mengupdate jam setiap detik (rekursif)
+        setTimeout(updateClock, 1000);
+    }
+
+    // Memanggil fungsi updateClock() untuk pertama kali
+    updateClock();
 });
-
-const desktop = document.querySelector("#icons");
-desktop.addEventListener("click", e => {
-	triggers.forEach(trigger=>{
-		trigger.classList.remove("active");
-	});
-	start.checked = false;
-	start.removeAttribute("checked");
-});
-
-const taskbar = document.querySelector("#window-taskbar");
-const close = taskbar.querySelector("[data-close]");
-close.removeAttribute("disabled");
-const applies = [... document.querySelectorAll("[data-apply]")];
-const global = [... document.querySelectorAll("[data-global-option]")];
-const options = [... document.querySelectorAll("[data-option]")];
-const outline = document.querySelector("#start-menu-outline");
-options.forEach(option => {
-	option.addEventListener("click", e => {
-		applies[1].removeAttribute("disabled")
-	})
-})
-applies.forEach(apply => {
-	apply.addEventListener("click", e => {
-		options.forEach((option, index) => {
-			if(option.checked) {
-				global[index].checked = true;
-				global[index].setAttribute("checked", "");
-			} else {
-				global[index].checked = false;
-				global[index].removeAttribute("checked");
-			}
-		});
-	});
-});
-
-const bars = [... document.querySelectorAll(".menu-status-bar")];
-bars.forEach(bar=>{
-	bar.addEventListener("click", e => {
-		if(bar.checked) {
-			bar.setAttribute("checked", "");
-		} else {
-			bar.removeAttribute("checked");
-		}
-	});
-});
-
-const welcome = document.querySelector("#welcome");
-const welcomeInside = document.querySelector("#window-welcome .window-inside");
-errors.forEach(error=>{
-	let name = error.getAttribute("data-name");
-	let alert = document.querySelector(`#alert-${name}`);
-	let retry = alert.querySelector("[data-retry]");
-	error.addEventListener("click", e => {
-		if(error.checked) {
-			error.setAttribute("checked", "");
-			if(retry) retry.focus();
-			if(name == "blue-screen") {
-				document.addEventListener("keydown", e => {
-					if (e.key === "Enter" && error.checked) {
-						error.checked = false;
-						error.removeAttribute("checked");
-					} else if (e.key === "Control") {
-						error.checked = false;
-						error.removeAttribute("checked");
-						start.checked = false;
-						start.removeAttribute("checked");
-						triggers.forEach(trigger=>{
-							trigger.checked = false;
-							trigger.classList.remove("active");
-						});
-						welcome.checked = true;
-						welcome.setAttribute("checked", "");
-						welcome.classList.add("active");
-						welcomeInside.removeAttribute("style");
-					} else {
-						error.checked = false;
-						error.removeAttribute("checked");
-					}
-				});
-			}
-		} else {
-			error.removeAttribute("checked");
-		}
-	});
-});
-
-let order = 0;
-let layer = 10;
-triggers.forEach(trigger=>{
-	let name = trigger.getAttribute("data-name");
-	let window = document.querySelector(`#window-${name}`);
-	let inside = window.querySelector(".window-inside");
-	let tab = document.querySelector(`.tab-${name}`);
-	let menu = window.querySelector("[data-menu]");
-	let max = document.querySelector(`#${name}-maximize`);
-	let drag;
-	window.addEventListener("click", e => {
-		start.checked = false;
-		start.removeAttribute("checked");
-		if(!trigger.classList.contains("active")) {
-			triggers.forEach(trigger=>{
-				trigger.classList.remove("active");
-			});
-			trigger.classList.add("active");
-		}
-	});
-	trigger.addEventListener("click", e => {
-		start.checked = false;
-		start.removeAttribute("checked");
-		if(trigger.checked) {
-			trigger.setAttribute("checked", "");
-			triggers.forEach(trigger=>{
-				trigger.classList.remove("active");
-			});
-			trigger.classList.add("active");
-			order = order + 1;
-			if(tab) tab.style.order = order;
-			layer = layer + 1;
-			window.style.zIndex = layer;
-			window.setAttribute("data-index", layer);
-			drag = new Draggabilly(window, {
-				handle: ".window-nav",
-				containment: true
-			});
-			if(window == taskbar) close.focus();
-		} else {
-			trigger.classList.remove("active");
-			trigger.removeAttribute("checked");
-			inside.removeAttribute("style");
-			window.removeAttribute("data-index");
-			if(drag != undefined) drag.destroy();
-			window.removeAttribute("style");
-			if(menu) {
-				menu.checked = false;
-				menu.removeAttribute("checked");
-			}
-			if(max) max.checked = false;
-		}
-	});
-});
-
-const minimizers = [... document.querySelectorAll("[data-minimize]")];
-minimizers.forEach((minimizer, index)=>{
-	let name = minimizer.getAttribute("data-name");
-	let trigger = document.querySelector(`#${name}`);
-	let tab = document.querySelector(`.tab-${name} .activate`);
-	let window = document.querySelector(`#window-${name}`);
-	tab.addEventListener("click", e => {
-		triggers.forEach(trigger=>{
-			trigger.classList.remove("active");
-		});
-		trigger.classList.add("active");
-		let layers = [... document.querySelectorAll("[data-index]")];
-		if(layers.length > 1 && !minimizer.checked) {
-			let values = [];
-			layers.forEach(layer=>{
-				values.push(layer.getAttribute("data-index"));
-			});
-			let highest = Math.max(...values) + 1;
-			window.style.zIndex = highest;
-			window.setAttribute("data-index", highest);
-		}
-	});
-	minimizer.addEventListener("click", e => {
-		if(minimizer.checked) {
-			minimizer.setAttribute("checked", "");
-			trigger.classList.remove("active");
-		} else {
-			minimizer.removeAttribute("checked");
-			triggers.forEach(trigger=>{
-				trigger.classList.remove("active");
-			});
-			trigger.classList.add("active");
-		}
-	});
-});
-
-windows.forEach(window=>{
-	let menu = window.querySelector("[data-menu]");
-	let toggle = window.querySelector("[data-toggle]");
-	let labels =  [... window.querySelectorAll("[data-label]")];
-	if(toggle) {
-		toggle.addEventListener("pointerdown", e => {
-			menu.checked = true;
-			menu.setAttribute("checked", "");
-		});
-	}
-	if(labels.length > 0) {
-		labels.forEach(label => {
-			label.addEventListener("click", e => {
-				menu.checked = false;
-				menu.removeAttribute("checked");
-			});
-		});
-	}
-});
-
-const time = document.querySelector(".clock");
-const updateTime = () => {
-	let date = luxon.DateTime.fromJSDate(new Date()).toLocaleString(luxon.DateTime.TIME_SIMPLE);
-	time.innerHTML = date;
+function openChatApp() {
+    var chatApp = document.getElementById('chatApp');
+    chatApp.style.display = 'block';
 }
-updateTime();
-setInterval(() => updateTime(), 1000);
+
+function minimizeChatApp() {
+    var chatApp = document.getElementById('chatApp');
+    chatApp.style.display = 'none';
+}
+
+function closeChatApp() {
+    var chatApp = document.getElementById('chatApp');
+    chatApp.style.display = 'none';
+}
